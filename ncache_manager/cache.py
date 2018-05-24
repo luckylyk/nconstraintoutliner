ffc71dc5d2c7fa12e2c97dd4f1010b892212c43f
luckylyk <lionel.brouyere@gmail.com>
2018-05-24 22:42:14 +0200

    utils added for the futur manager

--- ncache_manager/cache.py
+++ ncache_manager/cache.py
@@ -1,5 +1,5 @@
 
-from maya import cmds
+from maya import cmds, mel
 
 
 def disconnect_cachenodes(cachenodes=None):
@@ -21,6 +21,33 @@ def disconnect_cachenodes(cachenodes=None):
     return attributes
 
 
+def list_connected_cachenodes(nodes=None):
+    nodes = nodes or cmds.ls(type=('nCloth', 'hairSystem'))
+    return cmds.listConnections(nodes, type='cacheFile')
+
+
 def reconnect_all_cachenodes(connections):
     for attribute, connected in connections.iteritems():
         cmds.connectAttr(attribute, connected)
+
+
+def record_ncache(
+        nodes=None, start_frame=0, end_frame=100, output_folder=None):
+    '''
+    this method is a wrap around the mel command doCreateNclothCache
+    it force an cache with one cache per geometry (containing all frame).
+    '''
+    nodes = nodes or cmds.ls(type=('nCloth', 'hairSystem'))
+    output_folder = output_folder or ''
+
+    cmds.select(nodes)
+    command = (
+        'doCreateNclothCache 5 {{ "0", "{start_frame}", "{end_frame}",'
+        '"OneFile", "1", "{output_folder}", "1", "", "0", "replace", "1", "1", '
+        '"1", "0", "1","mcx"}}').format(
+            start_frame=start_frame,
+            end_frame=end_frame,
+            output_folder=output_folder)
+
+    cache_nodes = mel.eval(command)
+    return cache_nodes
--- ncache_manager/version.py
+++ ncache_manager/version.py
@@ -2,13 +2,16 @@
 import os
 import json
 
+from maya import cmds
 
 INFOS_FILENAME = 'infos.json'
+VERSION_FOLDERNAME = 'version_{}'
+
 
 class CacheVersion(object):
-    def __init__(self, folder):
-        self.folder = folder
-        if not os.path.exists(os.path.join(self.folder, INFOS_FILENAME)):
+    def __init__(self, path):
+        self.path = path
+        if not os.path.exists(os.path.join(self.path, INFOS_FILENAME)):
             raise ValueError('Invalid version path')
 
         self.xml_files = self._get_xml_files()
@@ -16,15 +19,63 @@ class CacheVersion(object):
         self.infos = self._load_infos()
 
     def _load_infos(self):
-        with open(os.path.join(self.folder, INFOS_FILENAME), 'r') as info_file:
+        with open(os.path.join(self.path, INFOS_FILENAME), 'r') as info_file:
             return json.load(info_file)
 
     def _get_mcx_files(self):
         return [
-            os.path.join(self.folder, f) for f in os.listdir(self.folder)
+            os.path.join(self.path, f) for f in os.listdir(self.path)
             if f.endswith('.mcx')]
 
     def _get_xml_files(self):
         return [
-            os.path.join(self.folder, f) for f in os.listdir(self.folder)
+            os.path.join(self.path, f) for f in os.listdir(self.path)
             if f.endswith('.xml')]
+
+
+def list_available_version_paths(path):
+    versions = [
+        os.path.join(path, folder)
+        for folder in os.listdir(os.path.join(path, folder))
+        if os.path.exists(os.path.join(path, folder, INFOS_FILENAME))]
+    return sorted(versions, key=lambda x: os.stat(x).st_ctime)
+
+
+def list_available_versions(path):
+    return [CacheVersion(p) for p in list_available_version_paths(path)]
+
+
+def get_new_version_path(path):
+    increment = 0
+    version_path = os.path.join(
+        path, VERSION_FOLDERNAME + str(increment).zfill(3))
+
+    while os.path.exists(version_path):
+        increment += 1
+        version_path = os.path.join(
+            path, VERSION_FOLDERNAME + str(increment).zfill(3))
+
+    return version_path
+
+
+def create_version(
+        path=None, name=None, comment=None, nodes=None,
+        start_frame=0, end_frame=0):
+
+    os.makedirs(path)
+    infos = dict(
+        name=None, comment=None, nodes=None, start_frame=0, end_frame=0)
+
+    with open(path, 'w') as version_file:
+        json.dump(version_file, infos)
+
+
+def connect_version(version, nodes=None):
+    nodes = nodes or cmds.ls(type=('nCloth', 'hairSystem'))
+    for node in nodes:
+        for mcx_file in version.mcx_files:
+            if node.replace(":", "_") in mcx_file:
+                cmds.cacheFile(
+                    attachFile=True,
+                    fileName=os.path.basename(mcx_file),
+                    directory=os.path.dirname(mcx_file))
